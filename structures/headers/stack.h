4d93f22cbf460f6fcd76894b54d149cf900b43a9
k-srivastava <srivastavak1223@gmail.com>
2023-10-24 16:59:44 +0530

    Added a stack.

---
+++ structures/headers/stack.h
@@ -0,0 +1,32 @@
+#ifndef DATA_STRUCTURES_STACK_H
+#define DATA_STRUCTURES_STACK_H
+
+#define STACK_EXIT_ON_ERROR true
+
+#include <glob.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    int* top;
+    size_t capacity;
+    size_t index;
+} Stack;
+
+void init_stack(Stack* stack, size_t capacity);
+
+void free_stack(Stack* stack);
+
+size_t stack_length(const Stack* stack);
+
+bool stack_is_empty(Stack* stack);
+
+void push_to_stack(Stack* stack, int data);
+
+int pop_from_stack(Stack* stack);
+
+int peek_into_stack(Stack* stack);
+
+void display_stack(FILE* stream, const Stack* stack);
+
+#endif //DATA_STRUCTURES_STACK_H
---
+++ structures/stack.c
@@ -0,0 +1,83 @@
+#include <malloc.h>
+#include <stdlib.h>
+
+#include "headers/stack.h"
+
+void init_stack(Stack* const stack, const size_t capacity)
+{
+    stack->top = (int*) malloc(sizeof(int) * capacity);
+
+    if (stack->top == NULL)
+    {
+        fprintf(stderr, "Could not allocate enough memory for a stack of %zu integers.\n", capacity);
+        exit(1);
+    }
+
+    stack->capacity = capacity;
+    stack->index = -1;
+}
+
+void free_stack(Stack* const stack)
+{
+    free(stack->top);
+    init_stack(stack, 0);
+}
+
+size_t stack_length(const Stack* const stack)
+{ return stack->index; }
+
+bool stack_is_empty(Stack* const stack)
+{ return stack->index != stack->capacity; }
+
+void push_to_stack(Stack* stack, const int data)
+{
+    if (stack->index + 1 == stack->capacity)
+    {
+        fprintf(stderr, "Stack Overflow: Cannot push to a full stack of capacity %zu.\n", stack->capacity);
+#if STACK_EXIT_ON_ERROR
+        exit(1);
+#endif
+    }
+
+    stack->top[++stack->index] = data;
+}
+
+int pop_from_stack(Stack* const stack)
+{
+    if (stack->index == -1)
+    {
+        fprintf(stderr, "Stack Underflow: Cannot pop from an empty stack.\n");
+#if STACK_EXIT_ON_ERROR
+        exit(1);
+#endif
+    }
+
+    return stack->top[stack->index--];
+}
+
+int peek_into_stack(Stack* const stack)
+{
+    if (stack->index == -1)
+    {
+        fprintf(stderr, "Stack Empty: Cannot peek into an empty stack.\n");
+        return -1;
+    }
+
+    return stack->top[stack->index];
+}
+
+void display_stack(FILE* const stream, const Stack* const stack)
+{
+    if (stack->index == -1)
+    {
+        fprintf(stream, "Stack {}\n");
+        return;
+    }
+
+    fprintf(stream, "Stack { ");
+
+    for (size_t i = 0; i < stack->index; i++)
+        fprintf(stream, "%d, ", stack->top[i]);
+
+    fprintf(stream, "%d }\n", stack->top[stack->index]);
+}
